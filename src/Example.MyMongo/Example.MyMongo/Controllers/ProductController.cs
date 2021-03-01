3f3d2adc1b489ddb2ca382bbc374130b982553e8
Eduardo Veras <eduardo.veras@xpi.com.br>
2021-03-01 13:13:05 -0300

    Removed default controller, add product model and controller

---
+++ src/Example.MyMongo/Example.MyMongo/Controllers/ProductController.cs
@@ -0,0 +1,48 @@
+﻿using Example.MyMongo.Model;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using Microsoft.Extensions.Logging;
+
+
+namespace Example.MyMongo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly ILogger<ProductController> _logger;
+        
+
+        // GET: api/<ProductController>
+        [HttpGet]
+        public IEnumerable<Product> Get()
+        {
+            return new List<Product>();
+        }
+
+        // GET api/<ProductController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<ProductController>
+        [HttpPost]
+        public void Post([FromBody] Product data)
+        {
+        }
+
+        // PUT api/<ProductController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] Product data)
+        {
+        }
+
+        // DELETE api/<ProductController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
