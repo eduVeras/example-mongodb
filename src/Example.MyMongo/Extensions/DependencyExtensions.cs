6914dc137a430c8b727c9b10a5a8b576e42ae169
Eduardo Veras <edurveras@hotmail.com>
2022-01-14 15:02:19 -0300

    Updated DockerCompose, Services

---
+++ src/Example.MyMongo/Extensions/DependencyExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Example.MyMongo.Interfaces.Repositories;
+using Example.MyMongo.Interfaces.Services;
+using Example.MyMongo.Repositories;
+using Example.MyMongo.Services;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Example.MyMongo.Extensions
+{
+    public static class DependencyExtensions
+    {
+
+        public static IServiceCollection AddDependecies(this IServiceCollection services)
+        {
+            services.AddScoped<IProductService, ProductService>();
+            services.AddScoped<IProductRepository, ProductRepository>();
+            return services;
+        }
+    }
+}
--- src/Example.MyMongo/Startup.cs
+++ src/Example.MyMongo/Startup.cs
@@ -21,7 +21,9 @@ namespace Example.MyMongo
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
-            services.AddSwaggerConfiguration();
+            services
+                .AddSwaggerConfiguration()
+                .AddDependecies();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
